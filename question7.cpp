859f975ab1e2a123e15462ada4e65f7f5bc4036f
Daksh-404 <61038855+Daksh-404@users.noreply.github.com>
2020-04-27 10:24:52 +0600

    Update question7.cpp

--- question7.cpp
+++ question7.cpp
@@ -3,20 +3,21 @@ using namespace std;
 bool checkLattice(int*poset,int n)
 {
   int k;
-  for(k=0;k<n-1;k++)
+  for(k=0;k<n;k++)
   {
-    if(poset[n-1]%poset[k]!=0)
+    if(poset[n-1]%poset[k]!=0||poset[k]%poset[0]!=0)
     {
-      break;
+      return false;
     }
   }
-  if(k==n-1) return true;
-  else return false;
+  return true;
 }
 int main()
 {
   int n;
   int poset[1000];
+  cout<<endl<<"Name - Daksh Gupta"<<endl;
+  cout<<"Roll no. - 2019UCO1669"<<endl<<endl;
   cout<<"Enter number of elements in the POSET:\n";
   cin>>n;
   cout<<"Enter the elements of the POSET(in the order of divisibility):\n";
